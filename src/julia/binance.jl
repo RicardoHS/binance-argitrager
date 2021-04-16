a057f65d7777adbaa1dd32b68324cbabf62ea5f8
Ricardo Hortelano <ricardo.hortelano@gmail.com>
2021-04-16 20:23:36 +0200

    improve types wrangling

--- src/julia/binance.jl
+++ src/julia/binance.jl
@@ -12,18 +12,23 @@ StructTypes.StructType(::Type{BinanceAPIOrderBookJSON}) = StructTypes.Struct()
 
 struct BinanceAPIOrderBook
     lastUpdateId::Int64
-    bids::Vector{Vector{Float64}}
-    asks::Vector{Vector{Float64}}
+    bids::Matrix{Float64}
+    asks::Matrix{Float64}
 end
 
 struct BinanceAPIResponse
     url::String
     status::Integer
     response::Any
+    timestamp::DateTime
     elapsed::Millisecond
     used_w::Integer
 end
 
+struct BAPIResponseException <: Exception
+    status::Integer
+    response::BinanceAPIResponse
+end
 
 function bapi_endpoint()
     url = "https://api.binance.com/api/v3/"
@@ -33,24 +38,28 @@ end
 function bapi_get(url::String)
     time_i = now()
     request = HTTP.request("GET", url)
-    time_e = now() - time_i
-    return request, time_e
+    time_e = now()
+    elapsed = time_e - time_i
+    timestamp = time_e - div(elapsed,2)
+    return request, timestamp, elapsed
 end
 
 function bapi_map_orderbook(orderbook_json)
-    asks = map(x -> parse.(Float64, x), orderbook_json.asks)
-    bids = map(x -> parse.(Float64, x), orderbook_json.bids)
+    asks_v = map(x -> parse.(Float64, x), orderbook_json.asks)
+    asks = transpose(hcat(asks_v...))
+    bids_v = map(x -> parse.(Float64, x), orderbook_json.bids)
+    bids = transpose(hcat(bids_v...))
     return BinanceAPIOrderBook(orderbook_json.lastUpdateId, bids, asks)
 end
 
 function bapi_get_orderbook(symbol::String, depth::Integer = 5)
     uri = "depth?symbol=$symbol&limit=$depth"
     url = bapi_endpoint() * uri
-    request, time_e = bapi_get(url)
+    request, timestamp, elapsed = bapi_get(url)
     parsed_json = JSON3.read(request.body, BinanceAPIOrderBookJSON)
     response = bapi_map_orderbook(parsed_json)
     headers = Dict(request.headers)
-    return BinanceAPIResponse(url, request.status, response, time_e, parse(Int32, headers["x-mbx-used-weight"]))
+    return BinanceAPIResponse(url, request.status, response, timestamp, elapsed, parse(Int32, headers["x-mbx-used-weight"]))
 end
 
 function bapi_get_exchangeinfo()
@@ -62,6 +71,6 @@ end
 
 function bapi_symbols()
     request = bapi_get_exchangeinfo()
-    symbols = [[d["symbol"], d["baseAsset"], d["quoteAsset"]] for d in json["symbols"] if d["status"]=="TRADING"]
+    symbols = [[d["symbol"], d["baseAsset"], d["quoteAsset"]] for d in request["symbols"] if d["status"]=="TRADING"]
     return symbols
 end
--- src/julia/core.jl
+++ src/julia/core.jl
@@ -2,8 +2,8 @@ using LinearAlgebra
 
 struct Order
     type::String
-    currency1::String
-    currency2::String
+    asset1::String
+    asset2::String
     value::Float64
 end
 
@@ -18,7 +18,11 @@ function compute_API(lambda_max::Real, n::Integer)
     return abs(lambda_max-n)/(n-1)
 end
 
-function find_arbitrage_path(A::Matrix, vecmax::Vector, currencies::Vector{String})
+function mean_weighted(a::Vector{Float64}, weights::Vector{Float64})
+    return sum(a .* weights) / sum(weights)
+end
+
+function find_arbitrage_path(A::Matrix, vecmax::Vector, assets::Vector{String})
     divide(i,j) = i/j
     vecmax = real(vecmax)
     B = [divide(i, j) for i in vecmax, j in vecmax]
@@ -30,52 +34,52 @@ function find_arbitrage_path(A::Matrix, vecmax::Vector, currencies::Vector{Strin
 
     if C_max[1]==C_min[2] && C_max[2]==C_min[1]
         # Direct arbitrage
-        push!(orders, Order("BUY", currencies[C_min[1]], currencies[C_min[2]], A[C_min]))
-        push!(orders, Order("SELL", currencies[C_max[1]], currencies[C_min[1]], A[C_max[1],C_min[1]]))
+        push!(orders, Order("BUY", assets[C_min[1]], assets[C_min[2]], A[C_min]))
+        push!(orders, Order("SELL", assets[C_max[1]], assets[C_min[1]], A[C_max[1],C_min[1]]))
         aer = 1 / ( C[C_min] * C[C_max] ) - 1
         return Arbitrage("DIRECT", orders, aer)
     elseif C_max[1]==C_min[1] || C_max[2]==C_min[2]
         # Triangular arbitrage
         if C_max[1]==C_min[1]
             # Arbitrage elements in the same row
-            push!(orders, Order("BUY", currencies[C_min[1]], currencies[C_min[2]], A[C_min]))
-            push!(orders, Order("SELL", currencies[C_min[1]], currencies[C_max[2]], A[C_min[1],C_max[2]]))
-            push!(orders, Order("BUY", currencies[C_min[2]], currencies[C_max[2]], A[C_min[2],C_max[2]]))
+            push!(orders, Order("BUY", assets[C_min[1]], assets[C_min[2]], A[C_min]))
+            push!(orders, Order("SELL", assets[C_min[1]], assets[C_max[2]], A[C_min[1],C_max[2]]))
+            push!(orders, Order("BUY", assets[C_min[2]], assets[C_max[2]], A[C_min[2],C_max[2]]))
             aer = 1 / C[C_min] * C[C_min[1],C_max[2]] / C[C_min[2],C_max[2]] - 1
             return Arbitrage("TRIANGULAR ROW", orders, aer)
         else # C_max[2]==C_min[2] 
             # Arbitrage elements in the same col
-            push!(orders, Order("BUY", currencies[C_min[1]], currencies[C_min[2]], A[C_min]))
-            push!(orders, Order("SELL", currencies[C_min[1]], currencies[C_max[1]], A[C_min[1],C_max[1]]))
-            push!(orders, Order("SELL", currencies[C_max[1]], currencies[C_min[2]], A[C_max[2],C_min[2]]))
+            push!(orders, Order("BUY", assets[C_min[1]], assets[C_min[2]], A[C_min]))
+            push!(orders, Order("SELL", assets[C_min[1]], assets[C_max[1]], A[C_min[1],C_max[1]]))
+            push!(orders, Order("SELL", assets[C_max[1]], assets[C_min[2]], A[C_max[2],C_min[2]]))
             aer = 1 / C[C_min] * C[C_min[1],C_max[1]] * C[C_max] - 1
             return Arbitrage("TRIANGULAR COLUMN", orders, aer)
         end
     else
         # Cuadrangular arbitrage
-        push!(orders, Order("BUY", currencies[C_min[1]], currencies[C_min[2]], A[C_min]))
-        push!(orders, Order("SELL", currencies[C_min[1]], currencies[C_max[1]], A[C_min[1],C_max[1]]))
-        push!(orders, Order("SELL", currencies[C_max[1]], currencies[C_max[2]], A[C_max[1],C_max[2]]))
-        push!(orders, Order("BUY", currencies[C_min[2]], currencies[C_max[2]], A[C_min[2],C_max[2]]))
+        push!(orders, Order("BUY", assets[C_min[1]], assets[C_min[2]], A[C_min]))
+        push!(orders, Order("SELL", assets[C_min[1]], assets[C_max[1]], A[C_min[1],C_max[1]]))
+        push!(orders, Order("SELL", assets[C_max[1]], assets[C_max[2]], A[C_max[1],C_max[2]]))
+        push!(orders, Order("BUY", assets[C_min[2]], assets[C_max[2]], A[C_min[2],C_max[2]]))
         aer = 1 / C[C_min] * C[C_min[1],C_max[1]] * C[C_max] / C[C_min[2],C_max[2]] - 1
         return Arbitrage("CUADRANGULAR", orders, aer)
     end
 end
 
-function arbitrage(cross_rates_matrix::Matrix, currencies::Vector{String})
+function arbitrage(cross_rates_matrix::Matrix, assets::Vector{String})
     eigens = eigen(cross_rates_matrix)
     nrow, ncol = size(eigens.vectors)
-    api = compute_API(eigens.values[ncol], ncol)
+    api = compute_API(real(eigens.values[ncol]), ncol)
 
-    if rank(cross_rates_matrix) != length(currencies)
-        println("NOT ARBITRAGE DETECTED, RANK(matrix) < LEN(currencies)")
+    if rank(cross_rates_matrix) != length(assets)
+        println("NOT ARBITRAGE DETECTED, RANK(matrix) < LEN(assets)")
         return nothing
     end
 
     if api > 0
         #println("ARBITRAGE DETECTED, API=",api)
         max_evector = eigens.vectors[:,ncol]
-        find_arbitrage_path(cross_rates_matrix, max_evector, currencies)
+        find_arbitrage_path(cross_rates_matrix, max_evector, assets)
     else
         println("NOT ARBITRAGE DETECTED, API=",api)
         return nothing
